4cc72ea9f2b874d8476ac4d755b80dbcecbc0961
加藤蒔朗 <rooji@katounoMacBook-Air.local>
2019-05-16 17:27:40 +0900

    devise-install

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,16 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    Tweet.create(tweet_params)
+    render action: :index
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body).merge(user_id: current_user.id)
   end
 end
